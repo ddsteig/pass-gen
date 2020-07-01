307f56eefacc1326c56122c4e9a5bbeeb1b32a8e
unknown <tekhexrax@yahoo.com>
2020-06-30 21:46:27 -0400

    Added 0 to numeric char array

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
   var lowcaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upcaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
-  var numericChar = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var numericChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
   
 // If user confirms a character type, it will be added to an array.
   var choices = [];
